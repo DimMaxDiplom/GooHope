6eea84cd08b0ab1e6de1208098bf748e5b54c588
ReF01L <salushkin1998@bk.ru>
2022-03-12 13:36:05 +1000

    Updated favicon and title for every page

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -2,11 +2,13 @@ import {createWebHistory, createRouter} from "vue-router"
 import MainPage from "@/components/pages/MainPage";
 import Login from "@/components/pages/Login";
 
+const DEFAULT_TITLE = 'GooHope'
+
 const routes = [
     {
         path: "/",
         name: MainPage.name,
-        component: MainPage
+        component: MainPage,
     },
     {
         path: "/login",
@@ -18,6 +20,13 @@ const routes = [
 const router = createRouter({
     history: createWebHistory(),
     routes
+})
+router.beforeEach((to, from, next) => {
+    let text = `${DEFAULT_TITLE} - ${to.name}`
+    if (to.name === MainPage.name)
+        text = `Добро пожаловать в ${DEFAULT_TITLE}`
+    document.title = text;
+    next();
 })
 
 export default router

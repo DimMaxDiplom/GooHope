3f64b796f4d482ddd72e0d59d865e50e23e3bb0f
ReF01L <salushkin1998@bk.ru>
2022-03-12 18:45:47 +1000

    Add vue-icon lib. Added Hero container on main page

--- client/src/components/pages/MainPage.vue
+++ client/src/components/pages/MainPage.vue
@@ -11,10 +11,10 @@
             <div class="hero_card_invite_title-title">Войдите в свой профиль для удобства</div>
           </div>
           <div class="hero_card_invite_btns">
-            <div class="hero_card_invite_btns-signin">Войти</div>
-            <div class="hero_card_invite_btns-signup">Зарегистрироваться</div>
+            <router-link to="/login" class="hero_card_invite_btns-signin">Войти</router-link>
+            <router-link to="/sign_up" class="hero_card_invite_btns-signup">Зарегистрироваться</router-link>
           </div>
-          <div class="hero_card_invite_adds">Хотите узнать больше?<router-link to="/" class="hero_card_invite_adds-link">сюда</router-link></div>
+          <div class="hero_card_invite_adds">Хотите узнать больше? Вам <router-link to="/" class="hero_card_invite_adds-link">сюда</router-link></div>
         </div>
         <div class="hero_card_main">
           <div class="hero_card_main-title">Популярные центры сообщества</div>
@@ -30,7 +30,7 @@
             </div>
             <div class="hero_card_main_games_game">
               <img src="" alt="" class="hero_card_main_games_game_image">
-              <div class="hero_card_main_games_game-name">Counter-Strike:Gl...</div>
+              <div class="hero_card_main_games_game-name">Counter-Strike: Gl...</div>
               <div class="hero_card_main_games_game_info">
                 <div class="hero_card_main_games_game_info-elem">5234 записей</div>
                 <div class="hero_card_main_games_game_info-elem">524 иллюстрации</div>
@@ -41,11 +41,13 @@
         </div>
         <div class="hero_card_search">
           <div class="hero_card_search_block">
-            <input type="search" class="hero_card_search_block-search">
+            <input type="text">
+            <vue-feather type="search" class="hero_card_search_block-search-icon" size="15"></vue-feather>
             <div class="hero_card_search_block-search">Найти людей</div>
           </div>
           <div class="hero_card_search_block">
-            <input type="search" class="hero_card_search_block-search">
+            <input type="text">
+            <vue-feather type="search" class="hero_card_search_block-search-icon" size="15"></vue-feather>
             <div class="hero_card_search_block-search">Найти продукт</div>
           </div>
         </div>
@@ -66,8 +68,204 @@ export default {
   width: 1250px
   & .hero
     &_title
+      width: 225px
+      height: 55px
+      position: relative
       &-title
         color: $orange
         font-size: 18px
-        +medium
+        +semi-bold
+        width: 224px
+        padding: 22px 0
+        border-bottom: 4px solid $orange
+        &::after
+          content: ''
+          display: block
+          position: absolute
+          right: -19px
+          bottom: -27px
+          width: 20px
+          height: 20px
+          background-color: $orange
+          -webkit-transform: rotate(-45deg)
+          -moz-transform: rotate(-45deg)
+          -ms-transform: rotate(-45deg)
+          -o-transform: rotate(-45deg)
+          transform: rotate(-45deg)
+          -webkit-transform-origin: 0 100%
+          -moz-transform-origin: 0 100%
+          -ms-transform-origin: 0 100%
+          -o-transform-origin: 0 100%
+          transform-origin: 0 100%
+    &_card
+      width: 100%
+      display: flex
+      justify-content: flex-start
+      align-items: flex-start
+      border-radius: 5px
+      background-color: $gray
+      height: 234px
+      margin-top: 45px
+      &_main
+        display: flex
+        flex-direction: column
+        justify-content: center
+        align-items: center
+        margin-right: 80px
+        &-title
+          color: $white
+          font-size: 24px
+          +medium
+          padding: 12px 2px 8px 0
+          border-bottom: 2px solid $orange
+        &_games
+          align-self: flex-start
+          display: flex
+          flex-direction: column
+          justify-content: center
+          align-items: flex-start
+          position: relative
+          &::before
+            content: ''
+            display: block
+            height: 131px
+            width: 2px
+            background-color: $white
+            position: absolute
+            // image size + name size + margin
+            left: calc(50px + 231px + 15px)
+            top: 25px
+          &_game
+            display: flex
+            justify-content: flex-start
+            align-items: center
+            &:nth-child(1)
+              padding: 25px 0
+            &_image
+              background-color: $white
+              width: 50px
+              height: 50px
+            &-name
+              margin: auto 7px
+              +medium
+              font-size: 24px
+              color: $white
+              width: 231px
+            &_info
+              display: flex
+              flex-direction: column
+              align-items: flex-start
+              justify-content: center
+              margin-left: 10px
+              &-elem
+                color: $white
+                +medium
+                font-size: 14px
+      &_invite
+        margin: 15px 70px auto 15px
+        display: flex
+        justify-content: center
+        align-items: flex-start
+        flex-direction: column
+        &_title
+          &-title
+            +light
+            color: $white
+            font-size: 18px
+            &:nth-child(2)
+              margin: 25px auto 30px auto
+        &_btns
+          display: flex
+          justify-content: space-between
+          align-items: center
+          width: 100%
+          &-signin
+            background-color: $black
+            border-radius: 10px
+            color: $white
+            font-size: 16px
+            padding: 8px 23px
+            text-align: center
+            text-decoration: none
+            +medium
+            position: relative
+            -webkit-transition: 0.4s
+            -moz-transition: 0.4s
+            -ms-transition: 0.4s
+            -o-transition: 0.4s
+            transition: 0.4s
+            &:hover
+              -webkit-transform: scale(1.1)
+              -moz-transform: scale(1.1)
+              -ms-transform: scale(1.1)
+              -o-transform: scale(1.1)
+              transform: scale(1.1)
+            &::after
+              content: ''
+              display: block
+              position: absolute
+              right: -65px
+              top: 50%
+              width: 65px
+              height: 4px
+              background-color: $orange
+          &-signup
+            background-color: $black
+            border-radius: 10px
+            color: $white
+            font-size: 16px
+            padding: 8px 10px
+            text-align: center
+            text-decoration: none
+            +medium
+            z-index: 10
+            -webkit-transition: 0.4s
+            -moz-transition: 0.4s
+            -ms-transition: 0.4s
+            -o-transition: 0.4s
+            transition: 0.4s
+            &:hover
+              -webkit-transform: scale(1.1)
+              -moz-transform: scale(1.1)
+              -ms-transform: scale(1.1)
+              -o-transform: scale(1.1)
+              transform: scale(1.1)
+        &_adds
+          padding-top: 20px
+          color: $white
+          font-size: 16px
+          +thin
+          &-link
+            +medium
+            color: $orange
+            text-decoration: underline
+      &_search
+        margin-top: 40px
+        &_block
+          position: relative
+          & input
+            background-color: $white
+            border-radius: 10px
+            border: none
+            outline: none
+            width: 210px
+            height: 35px
+            padding-left: 15px
+            +medium
+            font-size: 16px
+            color: $black
+            margin-bottom: 3px
+          &-search
+            color: $white
+            margin-bottom: 30px
+            +light
+            font-size: 12px
+            &-icon
+              color: $gray
+              position: absolute
+              right: 5px
+              bottom: 28px
+              &:hover
+                cursor: pointer
+
 </style>
--- client/src/main.js
+++ client/src/main.js
@@ -2,5 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from "@/router";
 import store from "@/vuex";
+import VueFeather from 'vue-feather'
 
-createApp(App).use(router).use(store).mount('#app')
+createApp(App).use(router).use(store).component(VueFeather.name, VueFeather).mount('#app')

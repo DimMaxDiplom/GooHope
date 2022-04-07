8f752e9ece548fe26731c9922a3b07772fd48a21
ReF01L <salushkin1998@bk.ru>
2022-04-08 00:57:06 +1000

    Updated dialog admin

--- api/socials/dialogs/admin.py
+++ api/socials/dialogs/admin.py
@@ -1,19 +1,5 @@
 from django.contrib import admin
-from .models import Dialog, Messages, Dialogs
+from .models import Dialog, Messages
 
-
-class DialogAdmin(admin.ModelAdmin):
-    list_display = ['created', 'updated', 'friend']
-
-
-class MessagesAdmin(admin.ModelAdmin):
-    list_display = ['dialog', 'message', 'sender']
-
-
-class DialogsAdmin(admin.ModelAdmin):
-    list_display = ['user', 'dialog']
-
-
-admin.site.register(Dialog, DialogAdmin)
-admin.site.register(Messages, MessagesAdmin)
-admin.site.register(Dialogs, DialogsAdmin)
+admin.site.register(Dialog)
+admin.site.register(Messages)
